a4ba2259e3380a5c3b517a3a23bf27be70d586fa
apurva-6265 <apurva18092003@gmail.com>
2024-07-24 00:32:24 +0530

    created user and video model with getting knowledge of jwt, bcrypt and aggregate packages

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-import express from "express";
-const app = express();
+import { asyncHandler } from "../utils/asyncHandlers.js";
 
 const connectDB = async () => {
   try {
---
+++ src/models/user.model.js
@@ -0,0 +1,92 @@
+import  {Schema, model} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, // Cloudinary url
+            required: true
+        },
+        avatar: {
+            type: String, // Cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId, // Schema is already destructured so no need to write mongoose before it.
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"]
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {timestamps: true}
+)
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")/*Give string input only*/) return next();
+    
+    this.password = await bcrypt.hash(this.password, 18);
+    next();
+}) // don't use arrow function in callback, because arrow function doesn't give you this functionality.
+
+userSchema.methods.isPasswordCorrect = 
+async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = model("User", userSchema);
